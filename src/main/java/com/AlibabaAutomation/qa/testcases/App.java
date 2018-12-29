f771d6e9dc087c1adc07445beeb00adc8a5287d0
Swap <Swap@Swap-PC>
2018-12-29 13:53:30 +0530

    FirstCheck

---
+++ src/main/java/com/AlibabaAutomation/qa/testcases/App.java
@@ -0,0 +1,13 @@
+package com.AlibabaAutomation.qa.testcases;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
